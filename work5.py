09b58535d46873c12ed282f04fb16bad2296e698
YYaFFF <protsievskii022@gmail.com>
2024-11-09 18:22:39 +0300

    Add files via upload

---
+++ work5.py
@@ -0,0 +1,52 @@
+from faker.generator import random
+from file_operations import VERSION
+import file_operations
+from faker import Faker
+fake=Faker("ru_Ru")
+skills=["Стремительный прыжок", "Электрический выстрел", "Ледяной удар", "Стремительный удар", "Кислотный взгляд", "Тайный побег", "Ледяной выстрел", "Огненный заряд"]
+skills_sample=random.sample(skills,3)
+# context = {
+#   "first_name": fake.first_name_male(),
+#   "last_name": fake.last_name_male(),
+#   "job": fake.job(),
+#   "town": fake.city(),
+#   "strength": random.randint(3,18),
+#   "agility": random.randint(3,18),
+#   "endurance": random.randint(3,18),
+#   "intelligence": random.randint(3,18),
+#   "luck": random.randint(3,18),
+#   "skill_1": skills_sample[0].replace("е","е͠"),
+#   "skill_2": skills_sample[1].replace("е","е͠"),
+#   "skill_3": skills_sample[2].replace("е","е͠")
+# }
+special_letters={
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+for letter in skills_sample[0]:
+  lette=special_letters[lettesr]
+  print("буква {} будет такой {}".format(special_letters,lette))
+
+
+file_operations.render_template("hero_card.svg", "result_card.svg", context)
